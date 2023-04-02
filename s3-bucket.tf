3dfda1afbf5e0abb94448b91c52977269b0bbaf5
Josué Velázquez Gen <jvelazquezg6@gmail.com>
2023-04-02 00:09:30 -0600

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,8 +1,9 @@
 module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "2.8.0"
 
-  bucket = "my-s3-bucket"
-  acl    = "private"
+  bucket        = "my-s3-bucket"
+  acl           = "private"
   bucket_prefix = var.prefix
 
   versioning = {
